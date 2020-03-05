91a3bf3d5a9a3a91cceb2434403bdc7c12bf0dd4
Justin Dowdy <Jdowdy9k@gmail.com>
2020-03-04 21:10:01 -0500

    added first page content and timer

--- script.js
+++ script.js
@@ -1,2 +1,49 @@
-var totalSeconds = 0;
+var totalSeconds = 90;
 var secondsElapsed = 0;
+var interval;
+var secondsLeft = totalSeconds - secondsElapsed;
+var status = "Working";
+var playButton = document.querySelector("#Startquiz");
+var secondsDisplay = document.querySelector("#Timer");
+
+
+function getFormattedSeconds() {
+    var secondsLeft = (totalSeconds - secondsElapsed)
+  
+    var formattedSeconds;
+  
+    if (secondsLeft < 10) {
+      formattedSeconds = "0" + secondsLeft;
+    } else {
+      formattedSeconds = secondsLeft;
+    }
+  
+    return formattedSeconds;
+  }
+
+function startTimer() {
+    interval = setInterval(function() {
+      secondsElapsed++;
+      renderTime();
+    }, 1000);
+  }
+
+function renderTime() {
+    secondsDisplay.textContent = ("Time: " + getFormattedSeconds());
+
+    if (secondsElapsed >= totalSeconds) {
+      if (status === "Working") {
+        alert("Time for a break!");
+      } 
+  
+      stopTimer();
+    }
+  }
+  function stopTimer() {
+    secondsElapsed = 0;
+    setTime();
+    renderTime();
+  }
+
+
+  playButton.addEventListener("click", startTimer);
