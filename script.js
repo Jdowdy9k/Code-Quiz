b4e9e717a2d0aad73ecdacaba284db3694326dc9
Justin Dowdy <Jdowdy9k@gmail.com>
2020-03-07 00:48:47 -0500

    added questions array and script and started score keeping

--- script.js
+++ script.js
@@ -1,49 +1,124 @@
-var totalSeconds = 90;
+var totalSeconds = 60;
 var secondsElapsed = 0;
 var interval;
-var secondsLeft = totalSeconds - secondsElapsed;
-var status = "Working";
-var playButton = document.querySelector("#Startquiz");
+var score = [0];
+var userScoreEl = document.getElementById("user-score");
+var questionHead = document.getElementById("questions");
+var answerChoices = document.getElementById("answers");
+var questionNumber = -1;
+var answer;
+var secondsLeft = (totalSeconds - secondsElapsed);
+var startButton = document.querySelector("#startQuiz");
+var submitBtn = document.querySelector("#submitBtn");
 var secondsDisplay = document.querySelector("#Timer");
+var quizArea = document.querySelector(".container")
+var questions = [
+  {
+    title: "Which tag would you use to code in Javascript?",
+    choices: ["<java>", "<body>", "<script>", "<img>"],
+    answer: "<script>"
+  },
+  {
+    title: "What would you use to enclose a function?",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "curly brackets"
+  },
+  {
+    title: "Which Library would be used exclusivly to style an object",
+    choices: ["bootstrap", "Jquery", "Java", "Richmond public"],
+    answer: "bootstrap"
+  },
+  {
+    title: "What identifier is used on a Class?",
+    choices: ["#", ".", "*", "&"],
+    answer: "."
+  },
+  {
+    title: "What is a DOM in JavaScript?",
+    choices: ["Data of Mine", "Document of Master", "Data Object Modal", "Document Object Model"],
+    answer: "Document Object Model"
+  },
 
+];
 
-function getFormattedSeconds() {
-    var secondsLeft = (totalSeconds - secondsElapsed)
-  
-    var formattedSeconds;
-  
-    if (secondsLeft < 10) {
-      formattedSeconds = "0" + secondsLeft;
-    } else {
-      formattedSeconds = secondsLeft;
-    }
-  
-    return formattedSeconds;
-  }
 
 function startTimer() {
-    interval = setInterval(function() {
-      secondsElapsed++;
-      renderTime();
-    }, 1000);
-  }
-
-function renderTime() {
-    secondsDisplay.textContent = ("Time: " + getFormattedSeconds());
-
-    if (secondsElapsed >= totalSeconds) {
-      if (status === "Working") {
-        alert("Time for a break!");
-      } 
-  
-      stopTimer();
-    }
-  }
-  function stopTimer() {
-    secondsElapsed = 0;
-    setTime();
+  document.getElementById("home").classList.add('d-none');
+  document.getElementById("quiz").classList.remove('d-none');
+  document.getElementById("highScoresheet").classList.remove('d-none')
+  interval = setInterval(function () {
+    secondsElapsed++;
     renderTime();
+  }, 1000);
+}
+function renderTime() {
+  var secondsLeft = (totalSeconds - secondsElapsed);
+  secondsDisplay.textContent = ("Time: " + secondsLeft);
+  if (secondsLeft === 0 || questionNumber === questions.length) {
+    clearInterval(interval);
+    setTimeout(displayScore, 500);
+    questionroll();
   }
+}
+
+function stopTimer() {
+  secondsElapsed = 0;
+  renderTime();
+}
+
+function questionroll() {
+  questionNumber++;
+  answer = questions[questionNumber].answer
+
+  questionHead.textContent = questions[questionNumber].title;
+  answerChoices.innerHTML = "";
+
+  var choices = questions[questionNumber].choices;
+
+  for (var i = 0; i < choices.length; i++) {
+    var nextChoice = document.createElement("button");
+
+    nextChoice.textContent = choices[i]
+    answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+  }
+}
+answerChoices.addEventListener("click", function (event) {
+  var pEl = document.getElementsByClassName("feedback")[0]
 
 
-  playButton.addEventListener("click", startTimer);
+  if (answer === event.target.textContent) {
+    pEl.innerHTML = "Correct!";
+    score ++;
+    showFeedback();
+  } else {
+    pEl.innerHTML = "Sorry, that's incorrect.";
+    score --;
+    showFeedback();
+  }
+  questionroll();
+});
+function hideFeedback() {
+  var pEl = document.getElementsByClassName("feedback")[0]
+  pEl.style.display = 'none'
+}
+
+function showFeedback() {
+  var pEl = document.getElementsByClassName("feedback")[0]
+  pEl.removeAttribute('style');
+}
+
+function displayScore() {
+  document.getElementById("quiz").classList.add('d-none');
+  document.getElementById("submit-score").classList.remove('d-none');
+  userScoreEl.textContent = "Your final score is " + score + "/5 in " + secondsElapsed + " seconds";
+}
+
+startButton.addEventListener("click", startTimer);
+startButton.addEventListener("click", questionroll);
+submitBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  addScore();
+
+  window.location.href = './highscores.html'
+});
+
