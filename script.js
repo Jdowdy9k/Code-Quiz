8d2232db18556789f7e31f988d0943a9bd99e137
Justin Dowdy <Jdowdy9k@gmail.com>
2020-03-09 21:21:24 -0400

    cleaned up and added to highscore sheet

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ var totalSeconds = 60;
 var secondsElapsed = 0;
 var interval;
 var score = [0];
+var userName = document.getElementById("userName");
 var userScoreEl = document.getElementById("user-score");
 var questionHead = document.getElementById("questions");
 var answerChoices = document.getElementById("answers");
 var questionNumber = -1;
-var answer;
-var secondsLeft = (totalSeconds - secondsElapsed);
+var secondsLeft;
 var startButton = document.querySelector("#startQuiz");
 var submitBtn = document.querySelector("#submitBtn");
+var resetButton = document.querySelector("#restartBtn");
+var clearButton = document.querySelector("clearBtn");
 var secondsDisplay = document.querySelector("#Timer");
-var quizArea = document.querySelector(".container")
+var quizArea = document.querySelector(".container");
+var scoreArea = document.querySelector("#score-list");
 var questions = [
   {
     title: "Which tag would you use to code in Javascript?",
@@ -41,37 +44,38 @@ var questions = [
 
 ];
 
-
+//starts timer and displays question/answer feild when user clicks start button
 function startTimer() {
   document.getElementById("home").classList.add('d-none');
   document.getElementById("quiz").classList.remove('d-none');
-  document.getElementById("highScoresheet").classList.remove('d-none')
+  document.getElementById("highScoresheet").classList.add('d-none')
   interval = setInterval(function () {
     secondsElapsed++;
-    renderTime();
+    secondsLeft = (totalSeconds - secondsElapsed);
+    secondsDisplay.innerHTML = ("Time: " + secondsLeft);
+    if (secondsLeft === 0) {
+      stopTimer()
+      return;
+    }
   }, 1000);
 }
-function renderTime() {
-  var secondsLeft = (totalSeconds - secondsElapsed);
-  secondsDisplay.textContent = ("Time: " + secondsLeft);
-  if (secondsLeft === 0 || questionNumber === questions.length) {
-    clearInterval(interval);
-    setTimeout(displayScore, 500);
-    questionroll();
-  }
-}
-
 function stopTimer() {
-  secondsElapsed = 0;
-  renderTime();
+  clearInterval(interval)
+  displayScore()
 }
 
+//runs question/answer array when user clicks start button
 function questionroll() {
   questionNumber++;
-  answer = questions[questionNumber].answer
+
+  answer = questions[questionNumber].answer;
 
   questionHead.textContent = questions[questionNumber].title;
   answerChoices.innerHTML = "";
+  if (questionNumber >= 4) {
+    stopTimer();
+    displayScore();
+  }
 
   var choices = questions[questionNumber].choices;
 
@@ -80,7 +84,9 @@ function questionroll() {
 
     nextChoice.textContent = choices[i]
     answerBtn = answerChoices.appendChild(nextChoice).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+
   }
+
 }
 answerChoices.addEventListener("click", function (event) {
   var pEl = document.getElementsByClassName("feedback")[0]
@@ -88,37 +94,53 @@ answerChoices.addEventListener("click", function (event) {
 
   if (answer === event.target.textContent) {
     pEl.innerHTML = "Correct!";
-    score ++;
+    score++;
     showFeedback();
   } else {
     pEl.innerHTML = "Sorry, that's incorrect.";
-    score --;
     showFeedback();
   }
   questionroll();
+
 });
+
+//subtle alert for user based on a correct/incorrect answer choice 
 function hideFeedback() {
   var pEl = document.getElementsByClassName("feedback")[0]
   pEl.style.display = 'none'
 }
-
 function showFeedback() {
   var pEl = document.getElementsByClassName("feedback")[0]
   pEl.removeAttribute('style');
 }
 
+//displays user score and displays input for username to be stored on highscore list
 function displayScore() {
   document.getElementById("quiz").classList.add('d-none');
   document.getElementById("submit-score").classList.remove('d-none');
-  userScoreEl.textContent = "Your final score is " + score + "/5 in " + secondsElapsed + " seconds";
+  userScoreEl.textContent = "Your final score is " + score + "/4 in " + secondsElapsed + " seconds";
+  var recScore = userScoreEl.textContent;
+  document.getElementById("highScoresheet").classList.remove('d-none')
+  myJSON = JSON.parse(recScore)
+  localStorage.setItem("scoreJSON", recScore);
 }
+function addScore() {
+  var text = localStorage.getItem("scoreJSON");
+  var obj = JSON.parse(text);
+  document.getElementById("score-list").innerHTML = obj.text;
+}
+function reset() {
+  document.getElementById("home").classList.add('d-none');
+  document.getElementById("quiz").classList.remove('d-none');
+  document.getElementById("highScoresheet").classList.add('d-none')
+}
+
+
 
 startButton.addEventListener("click", startTimer);
 startButton.addEventListener("click", questionroll);
-submitBtn.addEventListener("click", function (event) {
-  event.stopPropagation();
-  addScore();
+submitBtn.addEventListener("click", addScore);
+clearButton.addEventListener("click", clearscores);
+resetButton.addEventListener("click", reset);
 
-  window.location.href = './highscores.html'
-});
 
